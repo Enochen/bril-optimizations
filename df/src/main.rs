a05408e2be3b9d0fdae3288e752193a43318428d
Enochen <sirenochen@gmail.com>
2023-09-18 12:40:46 -0400

    do weird trait gymnastics

--- df/src/main.rs
+++ df/src/main.rs
@@ -10,7 +10,28 @@ use reaching_defs::ReachingDefs;
 mod live_vars;
 mod reaching_defs;
 
-fn print_node<T: DataFlowDisplay>(node: CFGNode, res: &DataFlowResult<T>, cfg: &CFG) {
+enum ResultWrapper {
+    ReachingDefs(DataFlowResult<ReachingDefs>),
+    LiveVars(DataFlowResult<LiveVars>),
+}
+
+impl ResultWrapper {
+    fn print_cfg(&self, cfg: &CFG) {
+        let dyn_print: Box<dyn Fn(usize)> = match self {
+            ResultWrapper::ReachingDefs(res) => Box::new(|i| {
+                print_node(CFGNode::Block(i), &cfg, res);
+            }),
+            ResultWrapper::LiveVars(res) => Box::new(|i| {
+                print_node(CFGNode::Block(i), &cfg, res);
+            }),
+        };
+        for i in 0..cfg.blocks.len() {
+            dyn_print(i);
+        }
+    }
+}
+
+fn print_node<T: DataFlowDisplay>(node: CFGNode, cfg: &CFG, res: &DataFlowResult<T>) {
     let format = |f: Option<&T>| f.map_or("N/A".to_string(), |set| set.generate_string(&cfg));
     let label = match node {
         CFGNode::Block(i) => &cfg.blocks.get(i).unwrap().label,
@@ -21,32 +42,30 @@ fn print_node<T: DataFlowDisplay>(node: CFGNode, res: &DataFlowResult<T>, cfg: &
     println!("   out: {}", format(res.out_map.get(&node)));
 }
 
+fn run_reaching_defs(cfg: &CFG) -> ResultWrapper {
+    ResultWrapper::ReachingDefs(run_worklist::<ReachingDefs>(&cfg))
+}
+
+fn run_live_vars(cfg: &CFG) -> ResultWrapper {
+    ResultWrapper::LiveVars(run_worklist::<LiveVars>(&cfg))
+}
+
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    let algorithm = args[1].as_str();
+    let alg_input = args[1].as_str();
+    let run_algorithm = match alg_input {
+        "reaching_defs" => run_reaching_defs,
+        "live_vars" => run_live_vars,
+        _ => {
+            eprintln!("Unknown command: {}", alg_input);
+            std::process::exit(1);
+        }
+    };
 
     let program = load_program_from_read(io::stdin());
     for function in program.functions {
         let cfg = generate_cfg(&function);
-        // This needs a better solution but Rust is confusing
-        match algorithm {
-            "reaching_defs" => {
-                let df_res = run_worklist::<ReachingDefs>(&cfg);
-                for i in 0..cfg.blocks.len() {
-                    print_node(CFGNode::Block(i), &df_res, &cfg);
-                }
-            }
-            "live_vars" => {
-                let df_res = run_worklist::<LiveVars>(&cfg);
-                for i in 0..cfg.blocks.len() {
-                    print_node(CFGNode::Block(i), &df_res, &cfg);
-                }
-            }
-            _ => {
-                eprintln!("Unknown command: {}", algorithm);
-                std::process::exit(1);
-            }
-        };
+        run_algorithm(&cfg).print_cfg(&cfg);
         // print_node(CFGNode::Return, &df_res, &cfg);
     }
     Ok(())
